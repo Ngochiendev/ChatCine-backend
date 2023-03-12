1cb2cf5d5ac474600012319899f5e827b04e21e4
hiendev <hiendev.official@gmail.com>
2023-03-12 21:02:44 +0700

    update

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # make sure you've copied the default file from the nginx site-enabled and edited the root directory to 
-#/home/site/wwwroot/public 
+#/home/site/wwwroot/public .
 cp /home/site/default /etc/nginx/sites-available/default
 service nginx reload
 
