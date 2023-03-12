a81d4174cf45c8ae4e59b8b19283e6df16b4def9
VÕ NGỌC HIỆN <ngochiendev@MacBook-Pro-cua-VO.local>
2023-03-12 21:00:45 +0700

    update startup.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
-#!/bin/bash ....
-
 # make sure you've copied the default file from the nginx site-enabled and edited the root directory to 
 #/home/site/wwwroot/public 
 cp /home/site/default /etc/nginx/sites-available/default
